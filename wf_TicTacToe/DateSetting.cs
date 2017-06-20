b5abdb84be5a0c55181503d1395b4f41578acb33
Boogie <ganz161@yandex.ru>
2017-06-20 14:34:11 +0300

    Work on Player profiles 1.0

--- wf_TicTacToe/DateSetting.cs
+++ wf_TicTacToe/DateSetting.cs
@@ -22,13 +22,36 @@ namespace wf_TicTacToe
 			age = n1;
 			}
 
-	static	private void CreateDateFile()
+	static	private void CreateDirectory()
 			{
-			StreamWriter write_text;  //Класс для записи в файл
+
+			/* StreamWriter write_text;  //Класс для записи в файл
 			FileInfo file = new FileInfo("C://config.txt");
 			write_text = file.AppendText(); //Дописываем инфу в файл, если файла не существует он создастся
 			write_text.WriteLine(login, " ", age); //Записываем в файл текст из текстового поля
 			write_text.Close(); // Закрываем файл
+			 */
+			DirectoryInfo TicTacToe = new DirectoryInfo(@"C:\");
+			try
+				{
+				TicTacToe.CreateSubdirectory("System");
+				TicTacToe.CreateSubdirectory(@"System\Profile");
+				}
+			catch (IOException e)
+				{
+				Console.WriteLine(e.Message);
+				}
 			}
+
+	static private void CreateProfile()
+		{
+		FileInfo profile = new FileInfo(@"F:\Profile.txt");
+		}
+
+	static private void CreatePlayer()
+		{
+		FileInfo profile = new FileInfo(@"F:\Profile.txt");
+		FileStream s = profile.Open(FileMode.Append, FileAccess.Read);
+		}
 		}
 	}
